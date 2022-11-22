f77a48e3e77bd6ea527ab505e6e9619080b1365b
Guilherme Silva <guilhermesilva@Guilhermes-MacBook-Pro.local>
2022-11-22 10:24:19 -0300

    initial project structure

---
+++ Beauttiee/Beauttiee/BeauttieeApp.swift
@@ -0,0 +1,20 @@
+//
+//  BeauttieeApp.swift
+//  Beauttiee
+//
+//  Created by Guilherme Silva on 22/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BeauttieeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
